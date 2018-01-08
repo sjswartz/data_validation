8f4d811bc23a50ee2ca994bb7f252a2f0691bde0
manzorz <gmanny@uw.edu>
2018-01-08 15:15:15 -0800

    added feather and wash capabilites

--- inventory_data_by_stage.R
+++ inventory_data_by_stage.R
@@ -1,9 +1,10 @@
 library(dplyr)
 library(magrittr)
 library(data.table)
+library(feather)
 
 indicator <- "wash"
-collapse_data_type <- "Rdata" #can be csv
+collapse_data_type <- "feather" #can be csv, Rdata or feather
 
 j <- ifelse(Sys.info()[1]=="Windows", "J:/", "/snfs1/")
 
@@ -11,7 +12,7 @@ j <- ifelse(Sys.info()[1]=="Windows", "J:/", "/snfs1/")
 path <- paste0(j, "LIMITED_USE/LU_GEOSPATIAL/collapsed/", indicator)
 collapse_data_type <- tolower(collapse_data_type)
 file_end <- paste0(".", collapse_data_type, "$")
-files <- list.files(path, pattern=file_end, full.names = T, ignore.case=T) %>% grep(value=T, pattern="IND_AHS", invert=T)
+files <- list.files(path, pattern=file_end, full.names = T, ignore.case=T) %>% grep(value=T, pattern="IND_AHS", invert=T) %>% grep(value=T, pattern="country", invert=T)
 details <- file.info(files)
 d <- details
 d$path <- rownames(d)
@@ -23,8 +24,14 @@ if (collapse_data_type == "rdata"){
   load(most_recent) #loads collapsed
 } else if (collapse_data_type == "csv"){
   collapsed <- read.csv(most_recent, stringsAsFactors = F)
+} else if (collapse_data_type == "feather"){
+  if (indicator == "wash"){
+    most_recent <- d[1:4, path]
+  }
+  feathers <- lapply(most_recent, read_feather)
+  collapsed <- rbindlist(feathers, fill=T, use.names=T)
 } else{
-  stop("This script only accepts Rdata and csv as collapse_data_type arguments.")
+  stop("This script only accepts Rdata, feather and csv as collapse_data_type arguments.")
 }
 
 #set common names to ones used in this code
@@ -50,7 +57,7 @@ uq[!(country %in% unique(data_inventory$country)), n_by_country := 0]
 uq <- uq[order(n_by_country)]
 uq <- uq[Stage == "1" | Stage == "2a" | Stage == '2b']
 setnames(uq, "n_by_country", "number_of_data_points")
-write.csv(uq, paste0(j, "temp/gmanny/data_inventory.csv"), row.names=F, na="")
+write.csv(uq, paste0(j, "temp/gmanny/", indicator, "data_inventory.csv"), row.names=F, na="")
 africa <- uq[Stage == "1"]
 stage_2 <- uq[Stage == "2a" | Stage == "2b"]
 
